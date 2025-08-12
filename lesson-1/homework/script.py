0760db6d72d9c632d4ea81351ee136de7fe10538
sardormeliboev <sardormeliboyev1987@gmail.com>
2025-08-13 00:08:53 +0500

    Create script.py

---
+++ lesson-1/homework/script.py
@@ -0,0 +1,27 @@
+1. Kvadratning bir tomoni berilgan. Uning perimetri va yuzasini toping.
+a = 5
+p = 4*a
+print(p)
+
+2. Doiraning diametri berilgan. Uning uzunligini (aylanasini) toping.
+d = 10
+L = d* 3,14
+print(L)
+
+3. Ikkita son a va b berilgan. Ularning o‘rtacha qiymatini toping.
+a = 10
+b = 20
+urta = (a+b)/2
+print(urta)
+
+4. Ikkita son a va b berilgan. Ularning yig‘indisi, ko‘paytmasi va har birining kvadratini toping.
+a = 10
+b = 20
+y = a+b
+k = a*b
+akv = a*a
+bkv = b*b 
+print(y)
+print(k)
+print(akv)
+print(bkv)
